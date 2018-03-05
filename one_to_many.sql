cc3ef95de249919cae13af58da56069386348fb0
Brian Haller <brianshaller@gmail.com>
2018-03-05 10:25:46 -0800

    commit

---
+++ one_to_many.sql
@@ -0,0 +1,49 @@
+-- create a new database
+CREATE DATABASE practice_customers;
+
+-- use the practice_customers database
+USE practice_customers;
+
+--double check you are in the correct database
+SELECT database();
+
+-- create a customer table and orders table
+CREATE TABLE customers(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    email VARCHAR(100)
+);
+
+CREATE TABLE orders(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    order_date DATE,
+    amount DECIMAL(8,2),
+    customer_id INT,
+    FOREIGN KEY(customer_id) REFERENCES customers(id)
+);
+
+-- insert some data into these tables
+INSERT INTO customers (first_name, last_name, email)
+VALUES ('Boy', 'George', 'george@gmail.com'),
+       ('George', 'Michael', 'gm@gmail.com'),
+       ('David', 'Bowie', 'david@gmail.com'),
+       ('Blue', 'Steele', 'blue@gmail.com'),
+       ('Bette', 'Davis', 'bette@aol.com');
+
+INSERT INTO orders (order_date, amount, customer_id)
+VALUES ('2016/02/10', 99.99, 1),
+       ('2017/11/11', 35.50, 1),
+       ('2014/12/12', 800.67, 2),
+       ('2015/01/03', 12.50, 2),
+       ('1999/04/11', 450.25, 5);
+
+--check that tables were created correctly
+DESC customers;
+DESC orders;
+
+--inner join customers and orders
+SELECT first_name, order_date, amount
+FROM customers
+JOIN orders
+ON customers.id = orders.customer_id;
